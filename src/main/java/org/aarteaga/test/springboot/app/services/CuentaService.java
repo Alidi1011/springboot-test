75d36df7b466a98ca8dc48306f0bfb10e67aeabf
Alisson <arteaga10alisson@gmail.com>
2023-08-21 10:22:13 -0500

    add unit test to controller and service layer

--- src/main/java/org/aarteaga/test/springboot/app/services/CuentaService.java
+++ src/main/java/org/aarteaga/test/springboot/app/services/CuentaService.java
@@ -3,9 +3,13 @@ package org.aarteaga.test.springboot.app.services;
 import org.aarteaga.test.springboot.app.models.Cuenta;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface CuentaService {
+    List<Cuenta> findAll();
     Cuenta findById(Long id);
+
+    Cuenta save(Cuenta cuenta);
     int revisarTotalTransferencias(Long bancoId);
     BigDecimal revisarSaldo(Long cuentaId);
     void transferir(Long numCuentaOrigen, Long numCuentaDestion, BigDecimal monto, Long bancoId);
--- src/test/java/org/aarteaga/test/springboot/app/SpringbootTestApplicationTests.java
+++ src/test/java/org/aarteaga/test/springboot/app/SpringbootTestApplicationTests.java
@@ -27,6 +27,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 class SpringbootTestApplicationTests {
@@ -135,5 +137,42 @@ class SpringbootTestApplicationTests {
 		assertEquals("Andrés", cuenta1.getPersona());
 		assertEquals("Andrés", cuenta2.getPersona());
 		verify(cuentaRepository, times(2)).findById(1L);
+	}
+
+	@Test
+	void testFindAll(){
+		//given
+		List<Cuenta> datos = Arrays.asList(crearCuenta001().orElseThrow(), crearCuenta002().orElseThrow());
+		when(cuentaRepository.findAll()).thenReturn(datos);
+		//when
+		List<Cuenta> cuentas = service.findAll();
+
+		//then
+		assertFalse(cuentas.isEmpty());
+		assertEquals(2, cuentas.size());
+		assertTrue(cuentas.contains(crearCuenta002().orElseThrow()));
+
+		verify(cuentaRepository).findAll();
+	}
+
+	@Test
+	void testSave(){
+		//given
+		Cuenta cuentaPepe = new Cuenta(null, "Pepe", new BigDecimal("3000"));
+		when(cuentaRepository.save(any())).then(invocation -> {
+			Cuenta c = invocation.getArgument(0);
+			c.setId(3L);
+			return c;
+		});
+
+		//when
+		Cuenta cuenta = service.save(cuentaPepe);
+		//then
+		assertEquals("Pepe", cuenta.getPersona());
+		assertEquals(3, cuenta.getId());
+		assertEquals("3000", cuenta.getSaldo().toPlainString());
+
+		verify(cuentaRepository).save(any());
+
 	}
 }
